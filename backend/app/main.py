29ca483d89302893fc1addec86af495d9b3dbebb
princek <princesamuelpks@gmail>
2026-05-06 13:36:28 +0000

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from .database import Base, engine
-from .routes import analysis, rules
+from .routes import analysis, dashboard, rules
 from fastapi.middleware.cors import CORSMiddleware
 
 from .analysis_metrics import collector
@@ -18,6 +18,7 @@ app.add_middleware(
 
 app.include_router(rules.router, prefix="/api")
 app.include_router(analysis.router, prefix="/api")
+app.include_router(dashboard.router, prefix="/api")
 
 
 @app.on_event("startup")
